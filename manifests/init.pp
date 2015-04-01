1d6dcf75f7b5b962a31547b785ce33e09a80d738
Luther Barnum <luther_barnum@jabil.com>
2015-04-01 14:21:26 -0400

    added params skeleton

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,9 @@
 #
 # Copyright 2015 Your name here, unless otherwise noted.
 #
-class logrotate {
+class logrotate (
+  $service_ensure = $logrotate::params::service_ensure,
 
+) inherits logrotate::params {
 
 }
