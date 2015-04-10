21ebe254f3575b426bd935cb387badad07107742
Luther Barnum <luther.barnum@gmail.com>
2015-04-09 22:51:37 -0400

    rewrote to use serverspec

--- spec/acceptance/logrotate_check_spec.rb
+++ spec/acceptance/logrotate_check_spec.rb
@@ -1,33 +1,37 @@
 require 'spec_helper_acceptance'
 
-describe 'apply logrotate module' do
-  it 'should work with no errors' do
-    pp = <<-EOS
-    class { 'logrotate': }
-    EOS
-
-    apply_manifest(pp, :catch_failures => true)
-    apply_manifest(pp, :catch_changes => true)
-  end
-end
-
-
-describe 'logrotate::config class' do
-  describe 'checking presence of config files' do
-    it 'should show that /etc/logrotate.conf is present' do
-      shell("ls /etc/logrotate.conf", :acceptable_exit_codes => 0)
-    end
-    it 'should show that /etc/cron.daily/logrotate is present' do
-      shell("ls /etc/cron.daily/logrotate", :acceptable_exit_codes => 0)
+describe 'logrotate module' do
+  describe 'apply logrotate module' do
+    it 'should work with no errors' do
+      pp = <<-EOS
+      class { 'logrotate': }
+      EOS
+  
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
     end
   end
-end
-
-describe 'logrotate::install class' do
-  describe 'checking presence of logrotate package' do
-    it 'should show that it is present' do
-      shell("rpm -q logrotate", :acceptable_exit_codes => 0)
+  
+  describe 'logrotate::install class' do
+    describe package('logrotate') do
+      it { should be_installed }
     end
   end
+  
+  describe 'logrotate::config class' do
+    describe file('/etc/logrotate.conf') do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode '644' }
+    end
+    describe file('/etc/cron.daily/logrotate') do
+      it { should be_file }
+      it { should be_owned_by 'root' }
+      it { should be_grouped_into 'root' }
+      it { should be_mode '755' }
+    end
+  end
+  
 end
-
+  
